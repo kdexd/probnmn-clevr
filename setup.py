2b2a3d382c57178c44e5f8ae985f2be7668b0f83
Karan Desai <karandesai281196@gmail.com>
2018-12-30 13:41:53 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='tbd',
+    version='1.0.0',
+    author='David Mascharka, Philip Tran, Ryan Soklaski, Arjun Majumdar',
+    url='https://github.com/davidmascharka/tbd-nets',
+    description='Transparency by Design: Closing the Gap Between Performance '
+                'and Interpretability in Visual Reasoning',
+    license='MIT',
+    zip_safe=True,
+)
