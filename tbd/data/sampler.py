d8d2d77243447f36e2149dec08676c2fe990799c
Karan Desai <karandesai281196@gmail.com>
2019-01-19 01:19:03 -0500

    Implement masking by max question length in sampler.

--- tbd/data/sampler.py
+++ tbd/data/sampler.py
@@ -8,19 +8,41 @@ class SupervisionWeightedRandomSampler(WeightedRandomSampler):
     with/without program supervision during question coding.
     """
 
-    def __init__(self, question_coding_dataset: Dataset):
+    def __init__(self,
+                 question_coding_dataset: Dataset,
+                 max_question_length: int = 45):
 
-        supervision_list = question_coding_dataset.get_supervision_list().float()
-        num_supervision = torch.sum(supervision_list)
-        num_no_supervision = len(question_coding_dataset) - num_supervision
+        self._supervision_list = question_coding_dataset.get_supervision_list().float()
 
-        # Set weights of indices for weighted random sampler.
-        weights = torch.zeros_like(supervision_list)
-        weights[supervision_list == 1] = 1 / num_supervision
-        weights[supervision_list == 0] = 1 / num_no_supervision
+        # Hold a list of question lengths, element is length of i-th question in CLEVR v1.0
+        # train split, excluding "@start@" and "@end@".
+        question_mask = question_coding_dataset._tokens.questions[:] != 0
+        # Shape: (699989, ) for CLEVR v1.0 train split
+        self._question_lengths = torch.tensor(question_mask.sum(-1)).long()
+        weights = self.update_curriculum(max_question_length)
 
         super().__init__(
             weights=weights,
             num_samples=len(question_coding_dataset),
             replacement=True,
         )
+
+    def update_curriculum(self, max_question_length):
+        supervision_mask = (self._question_lengths <= max_question_length).float()
+
+        # Examples with questions longer than specified will be temporarily dropped.
+        num_examples_in_curriculum = torch.sum(supervision_mask)
+        masked_supervision_list = self._supervision_list * supervision_mask
+
+        num_supervision = torch.sum(masked_supervision_list)
+        num_no_supervision = num_examples_in_curriculum - num_supervision
+
+        # Set weights of indices for weighted random sampler.
+        weights = torch.zeros_like(masked_supervision_list)
+        weights[masked_supervision_list == 1] = 1 / num_supervision
+        weights[masked_supervision_list == 0] = 1 / num_no_supervision
+
+        # Set probability of sampling masked examples as zero.
+        weights[supervision_mask == 0] = 0
+        self.weights = weights
+        return weights
