61310197ed30b5ddcfcdd5561a39b466773f6d72
Karan Desai <kddeskar@gmail.com>
2019-04-25 22:52:16 -0400

    Add trainer and evalutor for question coding, add scripts/train.py

--- probnmn/trainers/__init__.py
+++ probnmn/trainers/__init__.py
@@ -1,4 +1,4 @@
 from .program_prior_trainer import ProgramPriorTrainer
+from .question_coding_trainer import QuestionCodingTrainer
 
-
-__all__ = ["ProgramPriorTrainer"]
+__all__ = ["ProgramPriorTrainer", "QuestionCodingTrainer"]
