842f95ff029a5e3569187f6f944621af44525251
Karan Desai <kddeskar@gmail.com>
2020-03-10 14:13:14 -0400

    Bump to v2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="probnmn",
-    version="1.0.0",
+    version="2.0.0",
     author="Karan Desai",
     description="Code for ICML 2019 Paper: 'Probabilistic Neural-symbolic Models "
                 "for Interpretable Visual Question Answering'.",
